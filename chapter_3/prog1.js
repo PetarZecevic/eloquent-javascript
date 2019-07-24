67da7a773710a4bfae8fa03fbd1379ee675d4ce2
Petar Zecevic <pnz.vodka@gmail.com>
2019-07-24 20:31:13 +0200

    Done examples from chapter 3

--- chapter_3/prog1.js
+++ chapter_3/prog1.js
@@ -176,3 +176,64 @@ function example8()
     console.log(findSolution(24))
     console.log(findSolution(22))
 }
+
+// Growing functions.
+// Making functions out of repeatable code.
+function example9()
+{
+    console.log('\n\nGrowing functions')
+    // Number of cows and chickens are zero padded to length of 3.
+    function printFarmInventory(cows, chickens)
+    {
+        var cowString = String(cows)
+        while(cowString.length < 3)
+            cowString = '0' + cowString
+        console.log(cowString + ' Cows')
+        
+        var chickenString = String(chickens)
+        while(chickenString.length < 3)
+            chickenString = '0' + chickenString
+        console.log(chickenString + ' Chickens')
+    }
+
+    printFarmInventory(7, 11)
+    console.log('\n')
+
+    // One type of improvement.
+    function printZeroPaddedWithLabel(number, label)
+    {
+        var numberString = String(number)
+        while(numberString.length < 3)
+            numberString = '0' + numberString
+        console.log(numberString + ' ' + label)
+    }
+
+    function printFarmInventory1(cows, chickens, pigs)
+    {
+        printZeroPaddedWithLabel(cows, 'Cows')
+        printZeroPaddedWithLabel(chickens, 'Chickens')
+        printZeroPaddedWithLabel(pigs, 'Pigs')
+    }
+
+    printFarmInventory1(7, 11, 3)
+    console.log('\n')
+
+    // Another type of improvement.
+    function zeroPad(number, width)
+    {
+        var string = String(number)
+        while(string.length < width)
+            string = '0' + string
+        return string
+    }
+
+    function printFarmInventory2(cows, chickens, pigs)
+    {
+        console.log(zeroPad(cows, 3) + ' Cows')
+        console.log(zeroPad(chickens, 3) + ' Chickens')
+        console.log(zeroPad(pigs, 3) + ' Pigs')
+    }
+
+    printFarmInventory2(7, 16, 3)
+    console.log('\n')
+}
