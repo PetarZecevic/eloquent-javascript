3d4d46d307e7b807eba05bccc1ff3aba6d5a75ef
Petar Zecevic <pnz.vodka@gmail.com>
2019-07-31 21:40:27 +0200

    Add examples from chapter 4, last is Mutability

--- chapter_4/README.txt
+++ chapter_4/README.txt
@@ -3,5 +3,18 @@ What I learned:
 	returns undefined value.
 	- Array has push,pop methods and length property.
 	- Values of the type object are arbitrary collections of properties, and
-	we can add or remove these properties as we please
+	we can add or remove these properties as we please.
+	- If we try to access property that object doesn't have, undefined value is
+	returned.
+	- Object properties are written in JSON(Java Script Object Notation) format.
+	- Properties whose names are not valid variable names or valid numbers
+	have to be quoted.
+	- The binary in operator, when applied to a string and an object, returns a
+	Boolean value that indicates whether that object has that property.
+	- Arrays are just a kind of object specialized for storing sequences
+	of things.
+	- If property names are numbers you must use operator [] instead object_name.property_name.
+	- To access property with operator [] you must type object_name["property_name"], numbers are
+	exceptions, example: myObject[1], myObject["name"].
+	- Numbers,strings and Booleans are immutable.
 	
--- chapter_4/prog1.js
+++ chapter_4/prog1.js
@@ -14,16 +14,101 @@ function example1()
 // Methods
 function example2()
 {
-    console.log('Methods')
-    var doh = "Doh"
+    console.log('\nMethods')
+
+    var doh = 'Doh'
     console.log(typeof doh.toUpperCase())
     console.log(doh.toUpperCase())
 
     var mack = []
-    mack.push("Mack")
-    mack.push("the", "knife")
+    mack.push('Mack')
+    mack.push('the', 'knife')
     console.log(mack)
     console.log(mack.join(' '))
     console.log(mack.pop())
     console.log(mack)
+}
+
+// Objects
+function example3()
+{
+    console.log('\nObjects')
+
+    var day1 = {
+        squirrel : false,
+        events : ['events', 'touched tree', 'pizza', 'running', 'television']
+    }
+    
+    console.log(day1.squirrel)
+    console.log(day1.wolf)
+    // Adding property to object.
+    day1.wolf = false;
+    
+    console.log(day1.wolf)
+
+    var descriptions = {
+        work : 'Went to work',
+        'touched tree' : 'Touched a tree'
+    }
+
+    var anObject = {left : 1, right : 2}
+    var testObject = {1 : 'a', 2 : 'b', '3' : 'c'}
+
+    console.log('\n')
+    console.log(anObject.left, anObject.right)
+    console.log(anObject['left'], anObject['right'])
+    //console.log(anObject[left], anObject[right]) -> error.
+    //console.log(testObject.1, testObject.2) -> error.
+    // Remove property from object.
+    delete anObject.left
+    console.log(anObject.left)
+    console.log('left' in anObject)
+    console.log('right' in anObject)
+
+    console.log(testObject[1], testObject['2'], testObject[3])
+    console.log(1 in testObject)
+    console.log('1' in testObject)
+    console.log(3 in testObject)
+    delete testObject[1]
+    console.log(testObject[1])
+    console.log(1 in testObject)
+}
+
+// Array of objects.
+function example4()
+{
+    console.log('\nArray of objects')
+    var journal = [
+        {
+            events : ['work', 'touched tree', 'pizza', 
+                    'running', 'television'],
+            squirrel : false
+        },
+        {
+            events : ['work', 'ice cream', 'cauliflower', 'lasagna'],
+            squirrel : false
+        }
+    ]
+
+    console.log(journal[0])
+    console.log(journal[1])
+}
+
+// Mutability.
+function example5()
+{
+    console.log('\nMutability')
+
+    var object1 = {value : 10}
+    var object2 = object1
+    var object3 = {value : 10}
+
+    // Same references.
+    console.log(object1 == object2)
+    // Same values but not references.
+    console.log(object1 == object3)
+
+    object1.value = 0
+    console.log(object2.value)
+    console.log(object3.value)
 }
