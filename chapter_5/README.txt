26752eef78d9a1b74ba1a5831434eb41b6e9c423
Petar Zecevic <pnz.vodka@gmail.com>
2019-10-03 20:42:55 +0200

    Add second and third example(subchapter) from chapter 5

---
+++ chapter_5/README.txt
@@ -0,0 +1,7 @@
+What I learned:
+    - Functions that operate on other functions, either by taking them as
+    arguments or by returning them, are called higher-order functions.
+    
+    - JavaScript functions have an 'apply' method.
+    You pass it an array (or array-like object) of arguments, and it will call
+    the function with those arguments.
--- chapter_5/prog1.js
+++ chapter_5/prog1.js
@@ -40,3 +40,73 @@ function example1(){
 
     printMatrix([[1, 2, 3],[4, 5, 6]])
 }
+
+// Higher-order functions
+function example2(){
+    console.log('\nHigher-order functions\n')
+    
+    // Function used to create other function.
+    function greaterThan(n){
+        return function(m) { return m > n}
+    }
+
+    var greaterThan5 = greaterThan(5)
+    console.log(greaterThan5(6))
+    console.log(greaterThan5(4))
+    console.log('\n')
+
+    // Function that change other functions.
+    function noisy(f){
+        return function(arg){
+            var val = f(arg)
+            console.log('called with', arg, '- got ', val)
+            return val;
+        }
+    }
+
+    console.log(noisy(Boolean)(0))
+    console.log(noisy(Boolean)(2))
+    console.log(noisy(Number)('1'))
+    console.log('\n')
+
+    // Function that provide new types of control flow.
+    function unless(test, then){
+        if(!test){
+            then()
+        }
+    }
+    function repeat(times, body){
+        for(var i = 0; i < times; i++){
+            body(i)
+        }
+    }
+
+    repeat(10, function(n){
+        unless(n % 2, function(){
+            console.log(n , 'is even')
+        })
+    })
+
+}
+
+// Passing along arguments.
+function example3(){
+    console.log('\nPassing along arguments\n')
+
+    function transparentWrapping(f){
+        return function(){
+            // Passing all of given arguments to inner function.
+            return f.apply(null, arguments)
+        }
+    }
+
+    function argumentPrinter(){
+        for(var i = 0; i < arguments.length; i++){
+            console.log('Argument', i+1, ':', arguments[i])
+        }
+        console.log('\n')
+    }
+
+    transparentWrapping(argumentPrinter)(1, 2, 3, 4, 5)
+    transparentWrapping(argumentPrinter)('a', 3, 4, 'magic function')
+}
