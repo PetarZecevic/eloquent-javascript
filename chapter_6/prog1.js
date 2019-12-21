981ed17301ff9bef88bb85d87c15cc2cd49544d7
Petar Zecevic <Petar.Zecevic@rt-rk.com>
2019-12-21 18:37:40 +0100

    Add first exercise in chapter 6

--- chapter_6/prog1.js
+++ chapter_6/prog1.js
@@ -453,3 +453,54 @@ function example10(){
     console.log(dummy instanceof DummyClass)
     console.log(dummy instanceof Object)
 }
+
+// Vector.
+function exercise1(){
+    console.log('\nVector')
+
+    /**
+     * Class representing 2D vector.
+     * @param {first value(coordinate)} x 
+     * @param {second value(coordinate)} y 
+     */
+    function Vector(x, y){
+        this.x = x
+        this.y = y
+    }
+
+    /**
+     * Method used to add one vector to another by adding correspodent values
+     * and storing them in the new vector.
+     * Return value is the new vector.
+     */
+    Vector.prototype.plus = function(vector){
+        return new Vector(this.x + vector.x, this.y + vector.y)
+    }
+
+    /**
+     * Method used to subtract one vector from another by subtracting correspodent values
+     * and storing them in the new vector.
+     * The return value is the new vector.
+     */
+    Vector.prototype.minus = function(vector){
+        return new Vector(this.x - vector.x, this.y - vector.y)
+    }
+
+    /**
+     * Calculates the euclidean vector distance from the origin point of the same dimension the vector.
+     */
+    Object.defineProperty(Vector.prototype, 'length', {
+        get: function(){
+            return Math.sqrt(this.x**2 + this.y**2)
+        }
+    })
+
+    var vec = new Vector(1,2)
+    console.log(vec.x, vec.y, vec.length)
+
+    var result = vec.plus(new Vector(-1,1))
+    console.log(result.x, result.y, result.length)
+
+    result = vec.minus(new Vector(0, 5))
+    console.log(result.x, result.y, result.length)
+}
