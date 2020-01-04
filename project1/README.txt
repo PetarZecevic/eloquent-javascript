9763e4d7bf520acafa653c4dce04991d8570e96d
PetarZecevic <pnz.vodka@gmail.com>
2020-01-04 10:10:20 +0100

    PR1 - Add Vector class that represents each square in the grid

---
+++ project1/README.txt
@@ -0,0 +1,20 @@
+Description:
+Virtual ecosystem, a litle world populated with critters
+that move around and struggle for survival.
+
+Definition:
+To make this task manageable, we will radically simplify the concept of a world. 
+Namely, a world will be a two-dimensional grid where each entity takes up one 
+full square of the grid. 
+On every turn, the critters all get a chance to take some action.
+
+Characters:
+'#' - Represents walls and rocks
+'o' - Represents critters
+' ' - Represents empty space
+
+Example of the world:
+########
+##  o  #
+# o #  #
+########
--- project1/prog.js
+++ project1/prog.js
@@ -1,3 +1,48 @@
 function main(){
     console.log('\nProject: Electronic life')
 }
+
+// Representing space.
+/**
+ * Class representing 2D vector.
+ * @param {first value(coordinate)} x 
+ * @param {second value(coordinate)} y 
+ */
+function Vector(x, y){
+    this.x = x
+    this.y = y
+}
+
+/**
+ * Method used to add one vector to another by adding correspodent values
+ * and storing them in the new vector.
+ * Return value is the new vector.
+ */
+Vector.prototype.plus = function(vector){
+    return new Vector(this.x + vector.x, this.y + vector.y)
+}
+
+/**
+ * Method used to subtract one vector from another by subtracting correspodent values
+ * and storing them in the new vector.
+ * The return value is the new vector.
+ */
+Vector.prototype.minus = function(vector){
+    return new Vector(this.x - vector.x, this.y - vector.y)
+}
+
+/**
+ * Calculates the euclidean vector distance from the origin point of the same dimension the vector.
+ */
+Object.defineProperty(Vector.prototype, 'length', {
+    get: function(){
+        return Math.sqrt(this.x**2 + this.y**2)
+    }
+})
+
+/**
+ * Method used to pretty print vector properties.
+ */
+Vector.prototype.print = function(){
+    console.log('[', this.x, this.y, ']', 'length:', this.length)
+}
